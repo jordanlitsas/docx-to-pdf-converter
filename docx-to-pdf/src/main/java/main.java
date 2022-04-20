97d2bce9fa43fc53028fe71aa41032dc57c4a854
Jordan Litsas <jordan.litsas@gmail.com>
2022-04-20 13:54:03 +1000

    Converts .docx to .pdf with CLI

--- docx-to-pdf/src/main/java/main.java
+++ docx-to-pdf/src/main/java/main.java
@@ -3,7 +3,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
-
+import java.util.Scanner;
 import org.apache.poi.xwpf.converter.pdf.PdfConverter;
 import org.apache.poi.xwpf.converter.pdf.PdfOptions;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
@@ -15,8 +15,13 @@ public class main {
 
 
     public static void main(String[] args) {
-        try (InputStream is = new FileInputStream(new File("file.docx"));
-             OutputStream out = new FileOutputStream(new File("file.pdf"));) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter file name without its extension...");
+        String fileName = scanner.nextLine();
+
+        try (InputStream is = new FileInputStream(new File("inputFile/" + fileName + ".docx"));
+             OutputStream out = new FileOutputStream(new File("outputFile/" + fileName + ".pdf"));) {
             long start = System.currentTimeMillis();
             // 1) Load DOCX into XWPFDocument
             XWPFDocument document = new XWPFDocument(is);
@@ -24,7 +29,7 @@ public class main {
             PdfOptions options = PdfOptions.create();
             // 3) Convert XWPFDocument to Pdf
             PdfConverter.getInstance().convert(document, out, options);
-            System.out.println("rdtschools-Docx2PdfConversion-word-sample.docx was converted to a PDF file in :: "
+            System.out.println("DOCX was converted to a PDF file in :: "
                     + (System.currentTimeMillis() - start) + " milli seconds");
         } catch (Throwable e) {
             e.printStackTrace();
